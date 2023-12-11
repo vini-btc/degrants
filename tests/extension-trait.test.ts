0d4e665fd5a81dff3f96d93939837b602dd3dcff
Vini Andrade <me@vicnicius.com>
2023-12-11 08:01:26 +0100

    Remove trait tests
    Signed-off-by: Vini Andrade <me@vicnicius.com>

--- tests/extension-trait.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from 'vitest';
-import { initSimnet } from '@hirosystems/clarinet-sdk';
-
-describe('Extension Trait', async () => {
-  const simnet = await initSimnet();
-  it('ensures simnet is well initalised', () => {
-    expect(simnet.blockHeight).toBeDefined();
-  });
-});
